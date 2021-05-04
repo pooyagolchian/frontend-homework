be6793b9be218382ea51e194018e85cd6ff1316b
Pooya Golchian <pooya.golchian@gmail.com>
2021-05-04 19:29:10 +0430

    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react"
 import App from "./App"
-import { shallow } from "enzyme"
+import { shallow, mount } from "enzyme"
 import { Provider } from "react-redux"
 import store from "./store"
 
